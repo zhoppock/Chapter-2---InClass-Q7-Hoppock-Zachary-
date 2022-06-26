8a2c1b67128b63b907f65799e3ef17fc5381430c
Zachary Hoppock <61926087+zhoppock@users.noreply.github.com>
2022-06-26 02:58:37 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+//Hoppock, Zachary
+//aging.cpp
+//9/12/2020
+//Input details about your age 
+//Version #1
+#include <iostream>
+using namespace std;
+
+int main()
+{
+int age_now;
+int age_15_years;
+  cout << "What is your current age?\nInput ";
+  cin >> age_now;
+
+  cout << "How old will you be in 15 years?\nInput ";
+  cin >> age_15_years;
+  return 0;
+}
